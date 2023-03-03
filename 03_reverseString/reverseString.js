d7f6c4ebc9a2fee76684c180ff805c0568ba6e60
Ryan Anastasia <ryan.anastasia@huntclub.com>
2023-03-02 18:36:31 -0600

    Finish problem 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let newStr = ""
+    for (let i = 0; i < str.length; i++){
+        newStr += str[str.length-1-i]
+    }
+    return newStr
 };
 
 // Do not edit below this line
