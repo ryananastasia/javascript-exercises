d36ab5dee26b234e19bb71202c18f6dfce3c697a
Ryan Anastasia <ryan.anastasia@huntclub.com>
2023-03-02 22:08:53 -0600

    Finish problem 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
 const removeFromArray = function() {
-
+    let arr = arguments[0]
+    for (let i = 1; i < arguments.length; i++){
+        let curr = arr.indexOf(arguments[i])
+        if (curr > -1) {
+            arr = arr.slice(0,curr).concat(arr.slice(curr+1))
+        }
+    }
+    return arr 
 };
 
 // Do not edit below this line
