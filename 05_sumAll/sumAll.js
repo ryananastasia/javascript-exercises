6dabbf890a438cf5d7ba873776e6ece6e366e46a
Ryan Anastasia <ryan.anastasia@huntclub.com>
2023-03-04 14:22:40 -0600

    Finish problem 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    console.log(a, b)
+    console.log(typeof(a))
+    if (typeof(a) !== "number" || typeof(b) !== "number" || a < 0 || b < 0){
+        return "ERROR"
+    }
+    let sum = 0
+    if (a > b) {
+        [a, b] = [b, a]
+    }
+    for (let i = a; i < b+1; i++){
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
